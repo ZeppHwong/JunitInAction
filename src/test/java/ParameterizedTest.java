1635932e62d4fa2e3f3f2158c33e52e28b9093a2
caddelatte <kimheejoong@163.com>
2019-12-30 21:02:59 +0800

    junit

---
+++ src/test/java/ParameterizedTest.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class ParameterizedTest {
+    private double expectValue;
+    private double value1;
+    private double value2;
+
+    @Parameterized.Parameters
+    public static Collection<Integer[]> getTestParameters(){
+        return Arrays.asList(new Integer[][]{
+                {2, 1, 1},
+                {3, 2, 1},
+                {4, 3, 1}
+        });
+    }
+
+    public ParameterizedTest(double expectValue, double value1, double value2){
+        this.expectValue = expectValue;
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    @Test
+    public void sum(){
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(expectValue, calculator.add(value1, value2), 0);
+    }
+}
